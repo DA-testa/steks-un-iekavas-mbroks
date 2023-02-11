ac42a117ffd7438619d9ae5b59c9951d6ca907cb
Māris Broks <124582558+mbroks@users.noreply.github.com>
2023-02-11 09:48:00 +0000

    this is a commit test

--- main.py
+++ main.py
@@ -13,19 +13,51 @@ def find_mismatch(text):
     opening_brackets_stack = []
     for i, next in enumerate(text):
         if next in "([{":
+            opening_brackets_stack.append(Bracket(next,i))
             # Process opening bracket, write your code here
-            pass
+            
+            #pass
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
 
+            #brac = opening_brackets_stack.pop()
+            #print(brac)
+            #u = are_matching(Bracket(next,i),opening_brackets_stack.pop())
+            #print(u)
+            if len(opening_brackets_stack)==0 and i==0:
+                return i+1
+            if len(opening_brackets_stack)==0  and i!=0:
+                return i+1
+            else:
+                brac = opening_brackets_stack.pop()
+                if(are_matching(Bracket(next,i),brac)):
+                    break    
+
+    if len(opening_brackets_stack)!=0:
+        return i+1
+            #pass
+    return 0
 
 def main():
-    text = input()
+    mode = input()
+    if mode=="I":
+        text=input()
+    elif mode=="F":
+        text=input()
+
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
 
+    if len(text)==1:
+        print(1)
+    elif mismatch ==0:
+        print("Success")
+    else:
+        print(mismatch)
+
+
+    #print(text)
 
 if __name__ == "__main__":
     main()
